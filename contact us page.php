58ea85df8c1c91d1e773bc872270ebaf6f4a4e77
Nabilah1812 <148188352+Nabilah1812@users.noreply.github.com>
2023-10-17 15:26:13 +0800

    Add files via upload

---
+++ contact us page.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Contact Us Page</title>
+    </head>
+</html>
+
+
+<html>
+<head>
+<link rel= "stylesheet" type="text/css" href="stylecontact.css">
+
+</head>
+<body>
+
+<img align="right" style= "padding-right: 50px" style= "padding-top: 25px" src="account.png" href="#" 
+width="45" height="45">
+
+<div class="topnav">
+    <a class="back-button" href="javascript:history.back()"><b> < </b></a>
+    <a class="home" href="#home">Home</a>
+    <a href="#contact">Contact Us</a>
+</div><br><br>
+
+<p style= "font-size: 30px;" class="solid">Your charity can help others. Your donation has the power 
+to transform lives and make a lasting impact. Be the change you want to see in the world by giving generously.</p><br>
+
+<center><p style="font-size:20px;">Have enquires? Feel free to contact us on</p></center> <br><br>
+
+<table style="width:50%">
+  <tr>
+    <td><img src="email.png" alt="contactemail" width="50" height="50"></td>
+    <td> <p style="font-size:20px;" class="register"> <a href="#"  style="color: black;">easyfund@gmail.com</a></p></td>
+    <td><img src="phone.png" alt="phonenumber" width="50" height="50"></td>
+    <td> <p style="font-size:20px;" class="register">  <a href="#"  style="color: black;">+6 03 4621 2323</a></p></td>
+  </tr>
+ 
+  </tr>
+</table>
+
+
+
+</body>
+</html>
+
+
