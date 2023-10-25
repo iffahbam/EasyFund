cfdbcef35ca4163292f368d25c0d1147ca3bfb01
iffahbam <84954172+iffahbam@users.noreply.github.com>
2023-10-25 23:07:22 +0800

    Update adminHome.php

--- adminHome.php
+++ adminHome.php
@@ -1,4 +1,5 @@
 
+
 <!DOCTYPE html>
 <html lang = "en">
 <head>
@@ -16,18 +17,26 @@
 }
 
 .new-button {
-    position: fixed;
-    top: 80px;
-    left: 60px;
+    position: absolute;
+    display: flex;
+    align-items: center;
+    top: 80px; /* Adjust the top position as needed */
+    left: 60px; /* Adjust the right position as needed */
+    /*background-color: white; /* Add your desired background color */
+    color: black; /* Text color */
+    padding: 5px 10px; /* Adjust the padding as needed */
+    border-radius: 5px; /* Add rounded corners if desired */
+    cursor: pointer; /* Add a pointer cursor on hover */
 }
+
 
 .text {
     display: inline-block;
     vertical-align: middle;
-    margin-left: 10px;
-    margin-bottom: 20px;
     font-size: 20px; 
+    margin-left: 14px;
 }
+
 
 body {
   margin: 0;
@@ -65,24 +74,27 @@ body {
     margin-bottom: 0px;
   }
 
-  .box {
+.box {
     background-color: white;
     width: 30%;
-    height: 40%;
+    min-height: 200px;
     padding: 60px;
     border: 1px solid black;
     margin: 60px;
     text-align: center; 
+    border-radius: 15px;
+    letter-spacing: 1px;
   }
 
-  .box:nth-child(3) p {
+.box:nth-child(3) p {
     margin-bottom: 25px; /* Add space after the paragraph text in Box 3 */
   }
 
-  .loading-container {
+.loading-container {
   display: flex;
   justify-content: center; /* Center the loading bars horizontally */
   margin-right: 20px; /* Adjust the margin as needed */
+  margin-bottom: 60px;
 }
 
 .loading-bars {
@@ -104,7 +116,7 @@ body {
     border: 1px solid #000;
 }
 
-  .loading-fill {
+.loading-fill {
     height: 100%;
     background-color: #DA3A3A;
     width: 50%; /* Adjust the width as needed for the loading progress */
@@ -112,12 +124,13 @@ body {
   }
 
   .loading-text {
-  position: absolute;
-  top: -25px; /* Adjust the position as needed */
-  left: 0;
-  width: 100%;
-  text-align: center;
-  font-size: 20px;
+    position: absolute;
+    bottom: 35px; /* Adjust the value to add more space */
+    left: 0;
+    width: 100%;
+    text-align: center;
+    font-size: 20px;
+    letter-spacing: 1px;
 }
 
 .edit-icon {
@@ -132,6 +145,7 @@ body {
 </style>
 </head>
 <body>
+<?php echo $_SESSION["username"]?>
 
 <div class="profile-icon">
 <img src="profile.jpg" width="30" height="30" alt="Profile">
@@ -152,7 +166,8 @@ body {
 
 <div class="box-container">
     <div class="box">
-    <div class="edit-icon">&#9998;</div>
+    <a href="editdetails.php" style="text-decoration: none;"> 
+    <div class="edit-icon">&#9998;</div></a>
       <h2>Condelence For Imran Syakir</h2></br>
       <p>Heartfelt condolences to brother
         Imran, student from KICT whose mother has just passed away recently.</p>
@@ -188,6 +203,53 @@ body {
     </div>
   </div>
 </div>
+
+<div class="box-container">
+    <div class="box">
+    <a href="editdetails.php" style="text-decoration: none;"> 
+    <div class="edit-icon">&#9998;</div></a>
+      <h2>Condelence For Imran Syakir</h2></br>
+      <p>Heartfelt condolences to brother
+        Imran, student from KICT whose mother has just passed away recently.</p>
+    </div>
+    
+    <div class="box">
+    <div class="edit-icon">&#9998;</div>
+      <h2>Fire Relieve Fundraiser</h2></br>
+      <p>Sister Humaira, student from AIKOL just lost her house due to fire 
+          on May in Melaka. Lets donate to relieve her burden.</p>
+    </div>
+    <div class="box">
+    <div class="edit-icon">&#9998;</div>
+      <h2>Flood Victim</h2></br>
+      <p>We regret to inform you that brother Afiq from IRKHS has been 
+          caught in the devastating flood that has affected his home and belongings</p>
+    </div>
+  </div>
+
+  <div class="loading-container">
+  <div class="loading-bars">
+    <div class="loading-bar">
+    <div class="loading-text">RM650 of RM1000</div>
+      <div class="loading-fill"></div>
+    </div>
+    <div class="loading-bar">
+    <div class="loading-text">RM650 of RM1000</div>
+      <div class="loading-fill"></div>
+    </div>
+    <div class="loading-bar">
+    <div class="loading-text">RM650 of RM1000</div>
+      <div class="loading-fill"></div>
+    </div>
+  </div>
+</div>
+
+<script>
+        function addNewAnnouncement() {
+            // Redirect the user to addAnnouncement.php
+            window.location.href = "addAnnouncement.php";
+        }
+    </script>
 
 </body>
 </html>
