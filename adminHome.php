5d6da21aa438a5917c876d08958fe149c2261294
iffahbam <84954172+iffahbam@users.noreply.github.com>
2023-11-10 21:47:17 +0800

    Update adminHome.php

--- adminHome.php
+++ adminHome.php
@@ -1,50 +1,16 @@
-
-<?php
-
-function encrypt($data, $key, $iv) {
-    $encrypted = openssl_encrypt($data, 'aes-256-cbc', $key, 0, $iv);
-    return base64_encode($encrypted);
-}
-
-function decrypt($data, $key, $iv) {
-    $data = base64_decode($data);
-    return openssl_decrypt($data, 'aes-256-cbc', $key, 0, $iv);
-}
-
-
-$connect = new PDO("mysql:host=localhost;dbname=testing", "root", "");
-
-$query = "SELECT * FROM news ORDER BY id DESC";
-$statement = $connect->prepare($query);
-$statement->execute();
-
-if($statement->rowCount() > 0) {
-    $result = $statement->fetchAll();
-} 
-
-else {
-    $output .= '<tr><td>No Data Found</td></tr>';
-}
-
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EasyFund</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
+    <title>Admin</title>
     <style>
-
         body {
-            background-color:rgb(241, 228, 231);
+            background-color: rgb(241, 228, 231);
         }
 
         .profile-icon {
@@ -104,11 +70,11 @@ else {
             color: black;
         }
 
-        .column-container {
+        .container {
             display: flex;
-            gap: 20px; /* Adjust the gap between boxes */
-            justify-content: space-between; /* Distribute boxes evenly in 3 columns */
-            flex-wrap: wrap; /* Wrap to the next line if there are more than 3 boxes */
+            gap: 20px;
+            flex-wrap: wrap;
+            justify-content: space-between;
             padding: 30px;
         }
 
@@ -117,10 +83,12 @@ else {
           text-decoration: none; /* Optional: Remove underlines from links */
         }
 
-        .title {
-            font-size: 24px; /* Adjust the font size as needed */
-            margin-top: 40px;
-            margin-bottom: -60px;
+        .column-container {
+            display: flex;
+            gap: 20px; /* Adjust the gap between boxes */
+            justify-content: space-between; /* Distribute boxes evenly in 3 columns */
+            flex-wrap: wrap; /* Wrap to the next line if there are more than 3 boxes */
+            padding: 30px;
         }
 
         .boxCol {
@@ -136,14 +104,20 @@ else {
             position: relative;
         }
 
+        .boxCol h3 {
+            font-size: 22px;
+            font-weight: bold;
+            margin-top: 70px; /* Adjust the margin as needed to move the title downwards */
+        }
+
+
         .box-background {
             background-image: url('photo.jpg');
             background-size: cover;
             min-height: 200px;
-            padding: 80px 60px;
+            padding: 90px 60px;
             border: 1px solid black;
         }
-
 
         .loading-bar {
             width: 100%;
@@ -154,6 +128,8 @@ else {
             flex-direction: row;
             justify-content: flex-start;
             position: relative;
+            margin-top: 10px;
+            
         }
 
         .loading-fill {
@@ -167,11 +143,12 @@ else {
             color: #555; /* Set the color as needed */
             display: flex;
             justify-content: center; 
+            margin-top: -60px; 
         }
 
         .edit-icon {
             position: relative;
-            bottom: 120px;
+            bottom: 110px;
             left: 185px;
             font-size: 40px;
             cursor: pointer;
@@ -180,9 +157,8 @@ else {
     </style>
 </head>
 <body>
-    <?php echo $_SESSION["username"] ?>
-
-    <a href="adminProfile.php">
+<?php echo $_SESSION["username"] ?>
+<a href="adminProfile.php">
     <div class="profile-icon">
         <img src="profile.jpg" width="30" height="30" alt="Profile">
     </div>
@@ -200,45 +176,47 @@ else {
         <img src="plus.jpg" width="30" height="30" alt="Plus Icon">
         <span class="text">NEW</span>
     </div>
-
-    <div align="center"><?php echo $error; ?></div>
     <br><br><br>
+    
     <?php
-    $currentAmount = 500;
-if (isset($result)) {
-    echo '<div class="column-container" id="boxContainer">';
-    foreach ($result as $row) {
-        echo '<div class="boxCol box-background">'; 
-        echo '<a href="details.php?news_id=' . $row["id"] . '&title=' . urlencode($row["title"]) . '&description=' . urlencode($row["description"]) . '&currentAmount=' . $row["currentAmount"] . '&targetAmount=' . $row["targetAmount"] . '" class="boxCol">';
-        echo '<div class="edit-icon">&#9998;</div>';
-        echo '<h2 class="title">' . $row["title"] . '</h2>';
-        echo '<br><br><br><br>';
-        echo '<span class="number">RM' . $currentAmount . ' of RM' . $row["targetAmount"] . '</span>'; // Display current amount out of target amount
-        echo '</a>'; // Close the anchor element
-        echo '<div class="loading-bar">';
-        echo '<div class="loading-fill" style="width: ' . calculateProgress($row["targetAmount"]) . '%;"></div>';
+    include('connect.php');
+    $sqlSelect = "SELECT * FROM news ORDER BY id DESC";
+    $result = mysqli_query($conn, $sqlSelect);
+    $currentAmount = 500; // Fixed current amount
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        echo '<div class="column-container" id="boxContainer">';
+        while ($data = mysqli_fetch_array($result)) {
+            echo '<div class="boxCol box-background">';
+            echo '<a href="details.php?id=' . $data['id'] . '">';
+            echo '<div class="edit-icon">&#9998;</div>';
+            echo '<h3>' . $data['title'] . '</h3>';
+            echo '<br><br><br><br>';
+            echo '<span class="number">RM' . $currentAmount . ' of RM' . $data['targetAmount'] . '</span>';
+            echo '</a>';
+            echo '<div class="loading-bar">';
+            echo '<div class="loading-fill" style="width: ' . calculateProgress($currentAmount, $data['targetAmount']) . '%;"></div>';
+            echo '</div>';
+            echo '</div>';
+        }
         echo '</div>';
-        echo '</div>';
+    } 
+    
+    else {
+        echo '<p>No news found</p>';
     }
-    echo '</div>';
-}
 
-function calculateProgress($targetAmount) {
-    // Calculate the progress as a percentage
-    // You need to implement your own logic here based on the actual progress
-    // For example, you could calculate it as (currentAmount / targetAmount) * 100
-    $currentAmount = 500; // Replace with the actual current amount
-    $progress = ($currentAmount / $targetAmount) * 100;
+    function calculateProgress($currentAmount, $targetAmount) {
+        // Calculate the progress as a percentage
+        $progress = ($currentAmount / $targetAmount) * 100;
+        return $progress;
+    }
+    ?>
 
-    return $progress;
-}
-?>
-   
-   <script>
+    <script>
         function addNewAnnouncement() {
             window.location.href = "addAnnouncement.php";
         }
     </script>
-
 </body>
 </html>
